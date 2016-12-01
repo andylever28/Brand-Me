b5fd16d24e94479274b5b6c92a2f2d6acdb401d7
Andrew Lever <andylever28@gmail.com>
2016-12-01 13:39:01 -0500

    Linked both sign up pages with passed params. New users are created with Type hard coded.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  # before_action :authorize, except: [:create, :new]
+  before_action :authorize, except: [:create, :new]
   # GET /users
   # GET /users.json
   def index
