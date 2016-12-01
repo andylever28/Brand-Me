b275d5c4995f482dbb6922358e71e95b4f519129
Carmen Barron <carmen.barron@gmail.com>
2016-11-30 22:41:44 -0500

    stub out models in TwitterDatum model

--- app/models/twitter_datum.rb
+++ app/models/twitter_datum.rb
@@ -1,3 +1,26 @@
 class TwitterDatum < ApplicationRecord
   belongs_to :user
+
+  def make_them_tweet
+    # generate random string for user to tweet, and give it to them in some rendered view
+    # make them click "done!" once they've tweeted, so that we can THEN run the API
+  end
+
+  def run_twitter_api
+    # API call goes here--must be AFTER they've tweeted the random string we gave them in
+    # make_them_tweet
+  end
+
+  def check_last_tweet
+    # here check API value for last tweet against instance variable supplied to user as a
+    # random string to tweet
+    # If it checks out, go to write_to_twitterdatum_model.  If not, kick them in the teeth.
+  end
+
+  def write_to_twitterdatum_model
+    #only run this if they tweeted the instance variable of random string we told them to
+    #don't want to write to TwitterDatum table unless we know it's their own handle
+  end
+
+
 end
