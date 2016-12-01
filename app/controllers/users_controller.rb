2e8b46b61b40b931d23c0790d6a2e995295ff74e
Andrew Lever <andylever28@gmail.com>
2016-12-01 17:40:15 -0500

    twitter thangs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
-class UsersController < ApplicationController
+ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authorize, except: [:create, :new]
+  before_action :authorize, except: [:create, :new, :twitter]
   # GET /users
   # GET /users.json
   def index
@@ -68,6 +68,7 @@ class UsersController < ApplicationController
       format.json { head :no_content }
     end
   end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
