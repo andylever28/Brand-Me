4abf74eb88a86a2ca20ac6bbb2619662e7fbe26e
Andrew Lever <andylever28@gmail.com>
2016-11-30 20:24:07 -0500

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+
+  get 'page/home'
+  root 'page#home'
+end
