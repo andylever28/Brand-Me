4c142ecadd69bb21e65ad717cf79a33940aced77
Carmen Barron <carmen.barron@gmail.com>
2016-11-30 20:58:55 -0500

    create seed & seed local db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.delete_all
+
+user_list = [
+  ["Bobby", "Bobson", "bob@email.com","asdf", "fishing", "Ambassador", "Bob likes to fish!"],
+  ["Suzy", "Snowflake", "suzy@email.com", "asdf", "foodie", "Ambassador", "Suzy likes to eat!"],
+  ["Richard", "Richardson", "rich@email.com", "asdf", "surfing", "Ambassador", "Rich loves the water!"],
+  ["Fishing Gear Inc", "a", "info@fishinggear.com", "asdf", "b", "Brand", "We need you to catch fish in our gear!"]
+]
+
+user_list.each do |first_name, last_name, email, password, category, kind, summary|
+  User.create(first_name: first_name, last_name: last_name, email: email, password: password, category: category, kind: kind, summary: summary)
+end
