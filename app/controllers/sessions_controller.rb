4eca1c10b9d2e8ef7f82414c2b038a7c5c5565a3
Carmen Barron <carmen.barron@gmail.com>
2016-12-04 11:40:19 -0500

    minor changes with consideration of oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,14 @@
 class SessionsController < ApplicationController
   def new
-
   end
 
   def create
-
     user = User.find_by_email(params[:email])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to controller: 'users', action: 'show', id: user.id    
-else
+      redirect_to controller: 'users', action: 'show', id: user.id
+      else
       render :new
     end
   end
@@ -19,4 +17,5 @@ else
     session[:user_id] = nil
     redirect_to root_url, notice: 'Logged out'
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,18 @@
 Rails.application.routes.draw do
-
   resources :sessions, only: [:new, :create, :destroy]
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+    get 'signup', to: 'users#new', as: 'signup'
+    get 'login', to: 'sessions#new', as: 'login'
+    get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  # For when Twitter has successfully authenticated and sent us back to the app
+  # allows us to take response & save API keys for the user so we can use Twitter on their behalf
+  get 'auth/:provider/callback' , to: 'sessions#create'
 
   resources :users do
-  resources :twitter_datum
-end
+    resources :twitter_datum
+  end
 
   get 'page/home'
   root 'page#home'
+
 end
