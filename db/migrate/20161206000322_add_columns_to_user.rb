de0a381750bd1bba5874e33f5f1db4edecece8d8
Andrew Lever <andylever28@gmail.com>
2016-12-05 19:05:10 -0500

    add columns to user model

---
+++ db/migrate/20161206000322_add_columns_to_user.rb
@@ -0,0 +1,6 @@
+class AddColumnsToUser < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :age, :string
+    add_column :users, :gender, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161201023354) do
+ActiveRecord::Schema.define(version: 20161206000322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20161201023354) do
     t.text     "summary"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "age"
+    t.string   "gender"
   end
 
   add_foreign_key "twitter_data", "users"
