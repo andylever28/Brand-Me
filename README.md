1c772f5ed576fb73190533c4d39efc7d89bded2c
Carmen Barron <carmen.barron@gmail.com>
2016-12-08 18:01:18 -0500

    migrate mailer + gem install

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # README
 
+Heroku deployment:  https://brandmeetsambassador.herokuapp.com/
+
 Trello:  https://trello.com/b/Sh7rpeD7/pitch-day-fll7-carmen-andrew
 
 GitHub repo:  https://github.com/andylever28/Pitch1
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ApplicationMailer
-  default from: 'welcome@email.com'
+  default from: ENV["GMAIL_USERNAME"]
 
   def welcome_email(user)
     @user = user
-    @url = 'http://example.com/login'
+    @url = 'https://brandmeetsambassador.herokuapp.com/login'
     mail(to: @user.email, subject: 'Welcome!')
   end
 
