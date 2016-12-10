9962f201b7bbdcd857be9613958aa040b748a8ec
Andrew Lever <andylever28@gmail.com>
2016-12-10 18:44:45 -0500

    style brand form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :first_name, presence: true
-  validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   validates :category, presence: true
   validates :kind, presence: true
